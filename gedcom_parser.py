7d71d7391179ec7c1f4d8742a2c82ca7f4bcca59
David Wang <weihuaw@gmail.com>
2014-01-08 14:33:49 +1100

    Update gedcom_parser.py
    use stack of element to avoid hard coding depth

--- gedcom_parser.py
+++ gedcom_parser.py
@@ -3,38 +3,26 @@ import re
 import xml.etree.ElementTree as ET
 
 
-def create_node_by_line(line, hasChild=False):
+def create_node_by_line(line):
     if line[2] is None:
         return ET.Element(line[1])
     name = line[1]
     value = line[2].strip()
     id_pattern = re.compile(r'(@.*@)')
-    node_id = id_pattern.match(name) # the first one may be id 
+    node_id = id_pattern.match(name)  # the first one may be id
     if node_id:
         node_id = node_id.group(1)
         name = value
     else:
-        node_id = id_pattern.match(value) # the second one is id 
+        node_id = id_pattern.match(value)  # the second one is id
         if node_id:
             node_id = node_id.group(1)
     new_node = ET.Element(name.lower())
     if node_id:
         new_node.set('id', node_id)
-    elif hasChild:
-        new_node.set('value', value)
     else:
         new_node.text = value
     return new_node
-
-
-def AddNewNode(node_list, record, next_record):
-    new_node_level = record[0]
-    new_node = create_node_by_line(record, new_node_level < next_record[0])
-    if new_node_level > 0 and node_list[new_node_level - 1] is not None:
-        node_list[new_node_level - 1].append(new_node)
-    node_list[new_node_level + 1:
-              ] = [None for i in range(new_node_level + 1, len(node_list))]
-    node_list[new_node_level] = new_node
 
 
 def gedcom_parser(input_file, output_file):
@@ -48,29 +36,32 @@ def gedcom_parser(input_file, output_file):
 
     root = ET.Element('gedcom')
     m_re = re.compile(r'\s*(\d+)\s+(\S+)(\s+.*)*')
-    prev_record = None
-    # max depth of the sample file is 3 (0,1,2)
-    node_stack = [None, None, None, None]
+
+    Element_stack = [(-1, root)]
 
     for line in file_text:
         line_parsed = m_re.match(line.strip())
         if not line_parsed:
             continue
-        next_record = (int(line_parsed.group(1)),
+        new_record = (int(line_parsed.group(1)),
                        line_parsed.group(2), line_parsed.group(3))
-        if prev_record:
-            AddNewNode(node_stack, prev_record, next_record)
-            if next_record[0] == 0:
-                root.append(node_stack[0])
-        prev_record = next_record
+        new_Element = create_node_by_line(new_record)
+        
+        if new_record[0] > Element_stack[-1][0] and Element_stack[-1][1].text is not None:
+            Element_stack[-1][1].set('value', Element_stack[-1][1].text)
+            Element_stack[-1][1].text = None
 
-    if prev_record:
-        AddNewNode(node_stack, prev_record, next_record)
-        root.append(node_stack[0])
+        while new_record[0] <= Element_stack[-1][0]:
+            Element_stack.pop()
+        Element_stack[-1][1].append(new_Element)
+        Element_stack.append((new_record[0], new_Element))
 
     xml_tree = ET.ElementTree(root)
-    xml_tree.write(output_file)
-
+    try:
+        xml_tree.write(output_file)
+    except:
+        print('Error: Writing xml file failed')
+        return
 
 if __name__ == '__main__':
     if(len(sys.argv) != 3):
