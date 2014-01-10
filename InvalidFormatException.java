16f9b18fe2467a1dfae01871e15b4b3955587030
David Wang <weihuaw@gmail.com>
2014-01-10 13:05:18 +1100

    Create InvalidFormatException.java

---
+++ InvalidFormatException.java
@@ -0,0 +1,7 @@
+
+public class InvalidFormatException extends Exception {
+  public InvalidFormatException() { super(); }
+  public InvalidFormatException(String message) { super(message); }
+  public InvalidFormatException(String message, Throwable cause) { super(message, cause); }
+  public InvalidFormatException(Throwable cause) { super(cause); }
+}
